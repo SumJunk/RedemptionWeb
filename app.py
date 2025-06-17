4d106fe8f81083c7162470f6d57c5a7940dd1925
aidan <ahart35@charlotte.edu>
2025-06-17 12:09:02 -0400

    Render connections

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, render_template
 
 app = Flask(__name__)
@@ -7,4 +8,5 @@ def home():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5500)
+ port = int(os.environ.get('PORT', 5500))  #dynamic set, if not set 5500 otherwise.
+ app.run(debug=False, host='0.0.0.0', port=port) #binds to all public ip addresses, reachable from outside, debug=False turns off debug mode for safety internals/ reloader issues. 
